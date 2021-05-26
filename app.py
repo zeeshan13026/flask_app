ccd71529daf6cbd4a7c2548c1bdf16216d2df134
zeeshan13026 <shan.13026@gmail.com>
2021-05-26 15:12:25 +0530

    revert run.py file changes

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store,StoreList
 
-# from db import db
+from db import db
 
 app = Flask(__name__)
-# db.init_app(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = 'jose'
 api = Api(app)
 
-# @app.before_first_request
-# def create_table():
-#     db.create_all()
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 
 jwt = JWT(app,authenticate,identity)
@@ -32,8 +32,8 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+    # from db import db
+    # db.init_app(app)
     app.run(port=5000, debug=True)
 # items =[]
 #
