7e4096db9f2f68c31b0f95a48963ac98eede842d
zeeshan13026 <shan.13026@gmail.com>
2021-05-26 15:17:38 +0530

    sqlite commented

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from db import db
 
 app = Flask(__name__)
 db.init_app(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = 'jose'
